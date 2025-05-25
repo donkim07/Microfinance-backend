970d15de8a2f22e3c686e0f59894c9c1fb7315a5
Donald <donestomih@gmail.com>
2025-05-26 00:09:29 +0300

    Add route for language switching functionality, allowing users to change application language via the LanguageController, enhancing localization support in the application.

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LanguageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/signup', function () {
     return view('registration/signup');
 });
 Route::get('/login', function () {
     return view('registration/login');
 });
+
+// Language switching
+Route::get('language/{locale}', [LanguageController::class, 'switchLanguage'])->name('language.switch');
